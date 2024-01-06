933a6a4e9d879aaae54e4abcad443ea6d3f10551
Roberto919 <robertosysm@gmail.com>
2024-01-05 17:14:21 -0800

    simplifying file - eliminating unused references

--- pkg_dir/config/config.py
+++ pkg_dir/config/config.py
@@ -45,77 +45,8 @@ creds_file_path = os.path.join(package_dir, "config", "local", "credentials.yaml
 #### Data base file location
 data_dir_path = os.path.join(package_dir, "data")
 
-#### Results directory name
-process_results_dirname = "results"
-
-
-
-
-"-------------- SQL files -------------"
-
-## Postgresql files
-
-### Main directory
-postgre_files_path = os.path.join(package_dir, "sql", "postgres") + "/"
-
-### Working hours in doctor's agendas - path to file
-postgre_docs_hrs = os.path.join("docs_working_hours", "docs_working_hours_py.sql")
-
-### Information about the doctor's agendas - path to file
-postgre_query_fullagenda = os.path.join("full_docs_agendas", "full_docs_agendas_py.sql")
-
-### Data of appointments with their creation date - path to file
-postgre_query_apptsct = os.path.join("appointments_creations", "appts_creation.sql")
-
-### Income data - query to replicate the Income per type of service report
-postgres_income_syspc = os.path.join("income_data", "v_service_income_data.sql")
-
-### Doctor's information in the database
-postgres_docs_data = os.path.join("docs_data", "db_docs.sql")
-
-
-
-
-
-"----------------------------------------------------------------------------------------------------------------------"
-############################## Time zone parameters ####################################################################
-"----------------------------------------------------------------------------------------------------------------------"
-
-
-## Relevant time zones
-utc_tz = timezone('UTC')
-mexico_tz = timezone('Mexico/General')
-
-
-
-
-
-"----------------------------------------------------------------------------------------------------------------------"
-############################## Useful parameters #######################################################################
-"----------------------------------------------------------------------------------------------------------------------"
-
-
-## Translation of keywords from english to spanish
-word_translation = {
-
-    "months": {
-
-        'April': "Abril",
-        'August': "Agosto",
-        'December': "Diciembre",
-        'February': "Febrero",
-        'January': "Enero",
-        'July': "Julio",
-        'June': "Junio",
-        'March': "Marzo",
-        'May': "Mayo",
-        'November': "Noviembre",
-        'October': "Octubre",
-        'September': "Septiembre",
-
-    },
-
-}
+#### Data base file location
+pkl_dir_path = os.path.join(data_dir_path, "pickles")
 
 
 
