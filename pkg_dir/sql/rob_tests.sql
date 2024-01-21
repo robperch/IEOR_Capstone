35904c8a132099d703800cd626b3095f6cae0f97
Roberto919 <robertosysm@gmail.com>
2024-01-21 12:19:15 -0800

    testing in query and extracting info

--- pkg_dir/sql/rob_tests.sql
+++ pkg_dir/sql/rob_tests.sql
@@ -1,45 +1,39 @@
----- EXTRACTING ALL AGENDA ENTRIES IN THE SYSTEM
+SELECT
+       cita.citaid as appointment_id,
+       c.cestatusobs,
+       citaestado as appointment_status
+
+FROM cita
+
+         INNER JOIN citaestatus c ON cita.citaid = c.citaid AND cita.citaanio = c.citaanio
+
+
+WHERE citafecha >= '2023-01-01'
+  AND citafecha <= '2023-05-01'
+  AND c.cestatusobs ~ ':(.*)Confirmado'
+  AND citaestado = 'NO_PRESENTO'
+;
+
 
 
 SELECT cita.citaid as appointment_id,
        citafecha as appointment_date,
        citahorad as appointment_start_time,
        citahorah as appointment_end_time,
---        SUBSTRING(c.cestatusobs, '\((.+?)\)') as appointment_creation,
+       SUBSTRING(c.cestatusobs, '\((.+?)\)') as appointment_creation,
        c.cestatusobs,
        citaestado as appointment_status,
        u.usuarionomfull as doctor,
-       especialidad.especialidadnom as medical_specialty,
-       sucursal.sucursalnom as clinic,
-       p.pacienteid as patient_id,
-       p.pacientefnac as patient_birth_date
+       especialidad.especialidadnom as medical_specialty
 
 FROM cita
 
 
 INNER JOIN usuario u ON cita.citadoctorid = u.usuarioid
-INNER JOIN servicio ON cita.servicioid = servicio.servicioid
 INNER JOIN especialidad ON u.usuarioespecialidadid = especialidad.especialidadid
-INNER JOIN sucursal ON cita.citasucursalid = sucursal.sucursalid
-LEFT JOIN paciente p ON cita.pacienteid = p.pacienteid
 INNER JOIN citaestatus c ON cita.citaid = c.citaid AND cita.citaanio = c.citaanio
 
 
-WHERE citafecha >= '2000-11-03'
-  AND citafecha <= '2021-01-01'
---   AND c.cestatusobs ~* '\yCreado\y'
---   AND citaestado not in (
---                          'CANCELA_EMPLEADO',
---                          'CANCELA_PACIENTE',
---                          'BLOQUEADO',
---                          'DISPONIBLE',
---                          'TRIAGE',
---                          'TRIAGE_COMPLETO',
---                          'LISTA_ESPERA',
---                          'VALIDA_DATOS'
---                         )
-
-ORDER BY
-    citafecha DESC
-
+WHERE citafecha >= '2023-12-01'
+  AND citafecha <= '2023-12-02'
 ;
